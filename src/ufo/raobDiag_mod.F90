eaa8d4072fc031808c95c9fd5a43c91aeac3e472
Ricardo Todling <rtodling@thunder.gsfc.nasa.gov>
2017-11-15 18:23:29 +0000

    select raob only

--- src/ufo/raobDiag_mod.F90
+++ src/ufo/raobDiag_mod.F90
@@ -1,7 +1,7 @@
 module raobDiag_mod
 
-use m_diag_conv, only: diag_conv_header, diag_conv_mass
-use m_diag_conv, only: read_conv_diag_nc_header, read_conv_diag_nc_mass
+use m_diag_raob, only: diag_raob_header, diag_raob_mass
+use m_diag_raob, only: read_raob_diag_nc_header, read_raob_diag_nc_mass
 
 implicit none
 private
@@ -13,8 +13,8 @@ public :: raobDiag
 public :: raobDiag_read
 
 type :: raobDiag
-  type(diag_conv_header)            ::  header
-  type(diag_conv_mass),allocatable  ::  mass(:)
+  type(diag_raob_header)        ::  header
+  type(diag_raob_mass),pointer  ::  mass(:)
 end type raobDiag
 
 interface raobDiag_read  ; module procedure this_read_  ; end interface
@@ -30,14 +30,17 @@ integer(i_kind), intent(inout) :: nobs
 character(len=*),intent(in)    :: ncfname
 integer(i_kind) :: ier
 
-call read_conv_diag_nc_header(ncfname,self%header,nobs)
+call read_raob_diag_nc_header(ncfname,self%header)
+nobs=self%header%n_Observations_Mass
 allocate(self%mass(nobs))
-call read_conv_diag_nc_mass(ncfname,self%header,self%mass)
+call read_raob_diag_nc_mass(ncfname,self%header,self%mass,ier)
+nobs=self%header%n_Observations_Mass
 
 print*, myname_, ': Found this many observations: ', nobs
+print*, myname_, ': Size of type holding RAOB:    ', size(self%mass)
 print*, myname_, ': Date of input file:           ', self%header%date
 if(nobs>0)&
-print*, myname_, ': Mean observations ', sum(self%mass%Observation)/nobs
+print*, myname_, ': Mean observations:            ', sum(self%mass%Observation)/nobs
 
 end subroutine this_read_
 
