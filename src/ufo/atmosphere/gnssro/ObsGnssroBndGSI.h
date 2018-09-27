cc0d669f436609fed627805ac0fb6b9260824fe5
hailingz <hailingz@ucar.edu>
2018-09-27 21:58:30 +0000

    first commit of BndGSI forward operator

---
+++ src/ufo/atmosphere/gnssro/ObsGnssroBndGSI.h
@@ -0,0 +1,65 @@
+/*
+ * (C) Copyright 2017-2018 UCAR
+ * 
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+ */
+
+#ifndef UFO_ATMOSPHERE_GNSSRO_OBSGNSSROBNDGSI_H_
+#define UFO_ATMOSPHERE_GNSSRO_OBSGNSSROBNDGSI_H_
+
+#include <ostream>
+#include <string>
+
+#include <boost/scoped_ptr.hpp>
+
+#include "oops/base/Variables.h"
+#include "oops/util/ObjectCounter.h"
+#include "ufo/atmosphere/FortranAtmosphere.h"
+#include "ufo/ObsOperatorBase.h"
+
+namespace eckit {
+  class Configuration;
+}
+
+namespace ioda {
+  class ObsSpace;
+  class ObsVector;
+}
+
+namespace ufo {
+  class GeoVaLs;
+  class ObsBias;
+
+// -----------------------------------------------------------------------------
+
+/// GnssroBndGSI observation operator
+class ObsGnssroBndGSI : public ObsOperatorBase,
+                        private util::ObjectCounter<ObsGnssroBndGSI> {
+ public:
+  static const std::string classname() {return "ufo::ObsGnssroBndGSI";}
+
+  ObsGnssroBndGSI(const ioda::ObsSpace &, const eckit::Configuration &);
+  virtual ~ObsGnssroBndGSI();
+
+// Obs Operator
+  void simulateObs(const GeoVaLs &, ioda::ObsVector &, const ObsBias &) const;
+
+// Other
+  const oops::Variables & variables() const {return *varin_;}
+
+  int & toFortran() {return keyOperGnssroBndGSI_;}
+  const int & toFortran() const {return keyOperGnssroBndGSI_;}
+
+ private:
+  void print(std::ostream &) const;
+  F90hop keyOperGnssroBndGSI_;
+  const ioda::ObsSpace& odb_;
+  boost::scoped_ptr<const oops::Variables> varin_;
+};
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
+
+#endif  // UFO_ATMOSPHERE_GNSSRO_OBSGNSSROBNDGSI_H_
