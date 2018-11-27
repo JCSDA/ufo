843d6821c4e6d17a09f1a637ae5e098b93fff879
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2018-11-27 09:17:35 -0700

    changed channels parser test to read from config file

--- test/mains/TestChannelsParser.cc
+++ test/mains/TestChannelsParser.cc
@@ -5,14 +5,29 @@
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
  */
 
+
+#include "eckit/config/YAMLConfiguration.h"
 #include "eckit/exception/Exceptions.h"
+
+#include "oops/util/Logger.h"
 
 #include "ufo/utils/ChannelsParser.h"
 
 int main(int argc,  char ** argv) {
-  std::string s = "1, 7-11, 22, 5-8";
-  std::vector<int> channels = ufo::parseChannels(s);
-  std::vector<int> expected = {1, 5, 6, 7, 8, 9, 10, 11, 22};
+
+// Get configuration file from command line
+  ASSERT(argc >= 2);
+  eckit::PathName configfile = argv[argc - 1];
+
+// Read configuration
+  eckit::YAMLConfiguration config(configfile);
+
+  oops::Log::info() << "Configuration input file is: " << configfile << std::endl;
+  oops::Log::info() << "Full configuration is:"  << config << std::endl;
+
+  std::string chlist = config.getString("channels"); //"1, 7-11, 22, 5-8";
+  std::vector<int> channels = ufo::parseChannels(chlist);
+  std::vector<int> expected = config.getIntVector("parsed_channels"); //{1, 5, 6, 7, 8, 9, 10, 11, 22};
   ASSERT(std::equal(expected.begin(), expected.end(), channels.begin()));
   return 0;
 };
