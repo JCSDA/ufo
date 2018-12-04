cedb3857f76b294cac5fd5c26881a79ece29ef36
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2018-12-04 09:33:09 -0700

    refactored sea ice thickness

--- src/ufo/marine/seaicethickness/ufo_seaicethickness_tlad_mod.F90
+++ src/ufo/marine/seaicethickness/ufo_seaicethickness_tlad_mod.F90
@@ -1,54 +1,64 @@
 ! (C) Copyright 2017-2018 UCAR
-! 
+!
 ! This software is licensed under the terms of the Apache Licence Version 2.0
-! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
-!> Fortran module to handle ice concentration observations
+!> Fortran module for seaicethickness tl/ad observation operator
 
-module ufo_seaicethick_tlad_mod
+module ufo_seaicethickness_tlad_mod
 
-use iso_c_binding  
-use ufo_vars_mod
-use ufo_geovals_mod
-use kinds
-  
-implicit none
-public :: ufo_seaicethick_tlad
-public :: ufo_seaicethick_tlad_delete
-public :: ufo_seaicethick_tlad_settraj
-public :: ufo_seaicethick_simobs_tl
-public :: ufo_seaicethick_simobs_ad
-private
-integer, parameter :: max_string=800
+ use iso_c_binding
+ use config_mod
+ use kinds
 
-!> Fortran derived type for sea ice fraction observation operator
-type :: ufo_seaicethick_tlad
-   type(ufo_geoval) :: icethick !< ice thickness (traj)
-   type(ufo_geoval) :: icefrac  !< ice fraction  (traj)
-   logical :: ltraj = .false.   !< trajectory set?
-end type ufo_seaicethick_tlad
+ use ufo_geovals_mod, only: ufo_geovals, ufo_geoval, ufo_geovals_get_var
+ use ufo_basis_tlad_mod, only: ufo_basis_tlad
+ use ufo_vars_mod
+ use obsspace_mod
 
+ implicit none
+ private
 
-! ------------------------------------------------------------------------------
+ integer, parameter :: max_string=800
+
+ !> Fortran derived type for the tl/ad observation operator
+ type, extends(ufo_basis_tlad), public :: ufo_seaicethickness_tlad
+ private
+  type(ufo_geoval) :: icethick !< ice thickness (traj)
+  type(ufo_geoval) :: icefrac  !< ice fraction  (traj)
+ contains
+  procedure :: setup  => ufo_seaicethickness_tlad_setup
+  procedure :: delete  => ufo_seaicethickness_tlad_delete
+  procedure :: settraj => ufo_seaicethickness_tlad_settraj
+  procedure :: simobs_tl  => ufo_seaicethickness_simobs_tl
+  procedure :: simobs_ad  => ufo_seaicethickness_simobs_ad
+ end type ufo_seaicethickness_tlad
 
 contains
- 
-! ------------------------------------------------------------------------------
 
-subroutine ufo_seaicethick_tlad_delete(self)
+! ------------------------------------------------------------------------------
+subroutine ufo_seaicethickness_tlad_setup(self, c_conf)
 implicit none
-type(ufo_seaicethick_tlad), intent(inout) :: self
+class(ufo_seaicethickness_tlad), intent(inout) :: self
+type(c_ptr),              intent(in)    :: c_conf
+
+end subroutine ufo_seaicethickness_tlad_setup
+
+! ------------------------------------------------------------------------------
+subroutine ufo_seaicethickness_tlad_delete(self)
+implicit none
+class(ufo_seaicethickness_tlad), intent(inout) :: self
 
 self%ltraj = .false.
 
-end subroutine ufo_seaicethick_tlad_delete
+end subroutine ufo_seaicethickness_tlad_delete
 
 ! ------------------------------------------------------------------------------
-
-subroutine ufo_seaicethick_tlad_settraj(self, geovals)
+subroutine ufo_seaicethickness_tlad_settraj(self, geovals, obss)
 implicit none
-type(ufo_seaicethick_tlad), intent(inout) :: self
-type(ufo_geovals), intent(in)       :: geovals
+class(ufo_seaicethickness_tlad), intent(inout) :: self
+type(ufo_geovals),       intent(in)    :: geovals
+type(c_ptr), value,      intent(in)    :: obss
 
 character(len=*), parameter :: myname_="ufo_seaicethick_tlad_settraj"
 character(max_string) :: err_msg
@@ -65,16 +75,15 @@ self%icethick = icethick
 self%icefrac  = icefrac
 self%ltraj    = .true.
 
-end subroutine ufo_seaicethick_tlad_settraj
-
+end subroutine ufo_seaicethickness_tlad_settraj
 
 ! ------------------------------------------------------------------------------
-
-subroutine ufo_seaicethick_simobs_tl(self, geovals, hofx)
+subroutine ufo_seaicethickness_simobs_tl(self, geovals, hofx, obss)
 implicit none
-type(ufo_seaicethick_tlad), intent(in) :: self
-type(ufo_geovals), intent(in)    :: geovals
-real(c_double),  intent(inout) :: hofx(:)
+class(ufo_seaicethickness_tlad), intent(in)    :: self
+type(ufo_geovals),       intent(in)    :: geovals
+real(c_double),          intent(inout) :: hofx(:)
+type(c_ptr), value,      intent(in)    :: obss
 
 character(len=*), parameter :: myname_="ufo_seaicethick_simobs_tl"
 character(max_string) :: err_msg
@@ -113,15 +122,17 @@ do iobs = 1, size(hofx,1)
    enddo
 enddo
 
-end subroutine ufo_seaicethick_simobs_tl
+
+end subroutine ufo_seaicethickness_simobs_tl
 
 ! ------------------------------------------------------------------------------
-
-subroutine ufo_seaicethick_simobs_ad(self, geovals, hofx)
+subroutine ufo_seaicethickness_simobs_ad(self, geovals, hofx, obss)
 implicit none
-type(ufo_seaicethick_tlad), intent(in) :: self
-type(ufo_geovals), intent(inout) :: geovals
-real(c_double),  intent(inout)    :: hofx(:)
+class(ufo_seaicethickness_tlad), intent(in)    :: self
+type(ufo_geovals),       intent(inout) :: geovals
+real(c_double),          intent(in)    :: hofx(:)
+type(c_ptr), value,      intent(in)    :: obss
+
 
 character(len=*), parameter :: myname_="ufo_seaicethick_simobs_ad"
 character(max_string) :: err_msg
@@ -177,9 +188,9 @@ do iobs = 1, size(hofx,1)
       !print *, 'in ad: frac=', icefrac_d%vals(:,iobs)
    enddo
 enddo
-!hofx = 0.0
 
-!call abor1_ftn("end adjoint")
-end subroutine ufo_seaicethick_simobs_ad
+end subroutine ufo_seaicethickness_simobs_ad
 
-end module ufo_seaicethick_tlad_mod
+! ------------------------------------------------------------------------------
+
+end module ufo_seaicethickness_tlad_mod
