4ae801f45b2473bf68a4513e2a8a0fbbe5bf10ab
hailingz <hailingz@ucar.edu>
2019-01-31 13:51:59 -0700

    ropp2d tlad

---
+++ src/ufo/atmosphere/gnssro/BndROPP2D/ObsGnssroBndROPP2DTLAD.cc
@@ -0,0 +1,81 @@
+/*
+ * (C) Copyright 2017-2018 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "ufo/atmosphere/gnssro/BndROPP2D/ObsGnssroBndROPP2DTLAD.h"
+
+#include <ostream>
+#include <string>
+#include <vector>
+
+
+#include "ioda/ObsSpace.h"
+#include "ioda/ObsVector.h"
+
+#include "oops/base/Variables.h"
+#include "oops/util/Logger.h"
+
+#include "ufo/GeoVaLs.h"
+
+namespace ufo {
+
+// -----------------------------------------------------------------------------
+static LinearObsOperatorMaker<ObsGnssroBndROPP2DTLAD> makerGnssroBndROPP2DTL_("GnssroBndROPP2D");
+// -----------------------------------------------------------------------------
+
+ObsGnssroBndROPP2DTLAD::ObsGnssroBndROPP2DTLAD(const ioda::ObsSpace & odb,
+                                               const eckit::Configuration & config)
+  : keyOperGnssroBndROPP2D_(0), varin_(), odb_(odb)
+{
+  const eckit::LocalConfiguration obsOptions(config, "ObsOptions");
+  const eckit::Configuration * configc = &obsOptions;
+
+  ufo_gnssro_bndropp2d_tlad_setup_f90(keyOperGnssroBndROPP2D_, &configc);
+  const std::vector<std::string> vv{"temperature", "specific_humidity", "air_pressure"};
+
+  varin_.reset(new oops::Variables(vv));
+  oops::Log::info() << "ObsGnssroBndROPP2DTLAD vars: " << *varin_ << std::endl;
+  oops::Log::trace() << "ObsGnssroBndROPP2DTLAD created" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+ObsGnssroBndROPP2DTLAD::~ObsGnssroBndROPP2DTLAD() {
+  ufo_gnssro_bndropp2d_tlad_delete_f90(keyOperGnssroBndROPP2D_);
+  oops::Log::trace() << "ObsGnssroBndROPP2DTLAD destructed" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void ObsGnssroBndROPP2DTLAD::setTrajectory(const GeoVaLs & geovals, const ObsBias & bias) {
+  ufo_gnssro_bndropp2d_tlad_settraj_f90(keyOperGnssroBndROPP2D_, geovals.toFortran(), odb_);
+}
+
+// -----------------------------------------------------------------------------
+
+void ObsGnssroBndROPP2DTLAD::simulateObsTL(const GeoVaLs & geovals, ioda::ObsVector & ovec,
+                                      const ObsBiasIncrement & bias) const {
+  ufo_gnssro_bndropp2d_simobs_tl_f90(keyOperGnssroBndROPP2D_, geovals.toFortran(), odb_,
+                               ovec.size(), ovec.toFortran());
+}
+
+// -----------------------------------------------------------------------------
+
+void ObsGnssroBndROPP2DTLAD::simulateObsAD(GeoVaLs & geovals, const ioda::ObsVector & ovec,
+                                      ObsBiasIncrement & bias) const {
+  ufo_gnssro_bndropp2d_simobs_ad_f90(keyOperGnssroBndROPP2D_, geovals.toFortran(), odb_,
+                               ovec.size(), ovec.toFortran());
+}
+
+// -----------------------------------------------------------------------------
+
+void ObsGnssroBndROPP2DTLAD::print(std::ostream & os) const {
+  os << "ObsGnssroBndROPP2DTLAD::print not implemented" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
---
+++ src/ufo/atmosphere/gnssro/BndROPP2D/ObsGnssroBndROPP2DTLAD.h
@@ -0,0 +1,67 @@
+/*
+ * (C) Copyright 2017-2018 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#ifndef UFO_ATMOSPHERE_GNSSRO_BNDROPP2D_OBSGNSSROBNDROPP2DTLAD_H_
+#define UFO_ATMOSPHERE_GNSSRO_BNDROPP2D_OBSGNSSROBNDROPP2DTLAD_H_
+
+#include <ostream>
+#include <string>
+
+#include <boost/scoped_ptr.hpp>
+
+#include "oops/base/Variables.h"
+#include "oops/util/ObjectCounter.h"
+#include "ufo/atmosphere/gnssro/BndROPP2D/ObsGnssroBndROPP2DTLAD.interface.h"
+#include "ufo/LinearObsOperatorBase.h"
+
+// Forward declarations
+namespace eckit {
+  class Configuration;
+}
+
+namespace ioda {
+  class ObsSpace;
+  class ObsVector;
+}
+
+namespace ufo {
+  class GeoVaLs;
+  class ObsBias;
+  class ObsBiasIncrement;
+
+// -----------------------------------------------------------------------------
+/// GnssroBndROPP2D observation operator
+class ObsGnssroBndROPP2DTLAD : public LinearObsOperatorBase,
+                          private util::ObjectCounter<ObsGnssroBndROPP2DTLAD> {
+ public:
+  static const std::string classname() {return "ufo::ObsGnssroBndROPP2DTLAD";}
+
+  ObsGnssroBndROPP2DTLAD(const ioda::ObsSpace &, const eckit::Configuration &);
+  virtual ~ObsGnssroBndROPP2DTLAD();
+
+  // Obs Operators
+  void setTrajectory(const GeoVaLs &, const ObsBias &);
+  void simulateObsTL(const GeoVaLs &, ioda::ObsVector &, const ObsBiasIncrement &) const;
+  void simulateObsAD(GeoVaLs &, const ioda::ObsVector &, ObsBiasIncrement &) const;
+
+  // Other
+  const oops::Variables & variables() const {return *varin_;}
+
+  int & toFortran() {return keyOperGnssroBndROPP2D_;}
+  const int & toFortran() const {return keyOperGnssroBndROPP2D_;}
+
+ private:
+  void print(std::ostream &) const;
+  F90hop keyOperGnssroBndROPP2D_;
+  const ioda::ObsSpace& odb_;
+  boost::scoped_ptr<const oops::Variables> varin_;
+};
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
+#endif  // UFO_ATMOSPHERE_GNSSRO_BNDROPP2D_OBSGNSSROBNDROPP2DTLAD_H_
