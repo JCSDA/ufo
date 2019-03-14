58400b7eeaa3768d0ac11f152cc2e1a9e792b58e
hailingz <hailingz@ucar.edu>
2019-03-13 22:06:45 -0600

    geoval fails

--- src/ufo/gnssro/BndROPP2D/ufo_gnssro_bndropp2d_mod.F90
+++ src/ufo/gnssro/BndROPP2D/ufo_gnssro_bndropp2d_mod.F90
@@ -60,16 +60,16 @@ subroutine ufo_gnssro_bndropp2d_simobs(self, geovals, hofx, obss)
   type(ufo_geoval), pointer          :: t, q, prs, gph !, gph_sfc
   real(kind_real), allocatable       :: obsLat(:), obsLon(:), obsImpP(:), obsLocR(:), obsGeoid(:)
   integer                            :: iflip
-
+  integer, parameter :: n_horiz=31
   write(err_msg,*) "TRACE: ufo_gnssro_bndropp2d_simobs: begin"
   call fckit_log%info(err_msg)
 
+  print*,  "geovals%nobs & size(hofx)*n_horiz)", geovals%nobs, size(hofx)
 ! check if nobs is consistent in geovals & hofx
-  if (geovals%nobs /= size(hofx)) then
+  if (geovals%nobs /= size(hofx)*n_horiz) then
       write(err_msg,*) myname_, ' error: nobs inconsistent!'
       call abor1_ftn(err_msg)
   endif
-
 ! get variables from geovals
   call ufo_geovals_get_var(geovals, var_t,     t)         ! temperature
   call ufo_geovals_get_var(geovals, var_q,     q)         ! specific humidity
@@ -82,10 +82,16 @@ subroutine ufo_gnssro_bndropp2d_simobs(self, geovals, hofx, obss)
   nlev  = t%nval ! number of model levels
   nobs  = obsspace_get_nlocs(obss)
 
+print*, "printing 2d geovals =", t%nobs, t%vals(35:45,1)
+print*, "printing 2d geovals =", t%nobs, t%vals(35:45,16)
+print*, "printing 2d geovals =", t%nobs, t%vals(35:45,31)
+print*, "printing 2d geovals =", t%nobs, t%vals(35:45,32)
+print*, "printing 2d geovals =", t%nobs, t%vals(35:45,47)
+print*, "printing 2d geovals =", t%nobs, t%vals(35:45,62)
   iflip = 0
   if (prs%vals(1,1) .lt. prs%vals(prs%nval,1) ) then
     iflip = 1
-    write(err_msg,*) "TRACE: ufo_gnssro_bndropp2d_simobs:
+    write(err_msg,*) "TRACE: ufo_gnssro_bndropp2d_simobs:  &
                       Model vertical height profile is in descending order, &
                       but ROPP requires it to be ascending order, &
                       need flip"
