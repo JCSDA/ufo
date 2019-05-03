a5221748a453382319872d210015384f61a6ad6a
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2019-05-03 19:10:01 +0000

    Added/changed some ufo variable names

--- src/ufo/atmsfcinterp/ufo_atmsfcinterp_mod.F90
+++ src/ufo/atmsfcinterp/ufo_atmsfcinterp_mod.F90
@@ -51,32 +51,24 @@ subroutine ufo_atmsfcinterp_setup(self, c_conf)
   self%varout = config_get_string_vector(c_conf, max_string, "variables")
   !> Allocate varin: variables we need from the model
   allocate(self%varin(11))
-  !> Set vars_in based on vars_out
-  !do ii = 1, self%nvars
-  ! self%varin(ii) = self%varout(ii)
-  !enddo
-  ! TODO: additional variables will need to be added but all of the 'standard'
+  ! TODO: additional variables may need to be added but all of the 'standard'
   ! vars need the same input from the GeoVaLs (T,P,U,V,Q need all for each other)
   !> add geopotential height
-  self%varin(1) = "geopotential_height"
+  self%varin(1) = var_z
   !> need skin temperature for near-surface interpolations
-  self%varin(2) = "sfc_skin_temperature"
+  self%varin(2) = var_sfc_t
   !> need surface geopotential height to get difference from phi
-  self%varin(3) = "sfc_geopotential_height"
+  self%varin(3) = var_sfc_z 
   !> need surface roughness
-  self%varin(4) = "surface_roughness"
+  self%varin(4) = var_sfc_rough 
   !> need surface and atmospheric pressure for potential temperature
-  self%varin(5) = "surface_pressure"
-  self%varin(6) = "air_pressure"
-  self%varin(7) = "air_temperature"
-  self%varin(8) = "specific_humidity_mixing_ratio"
-  self%varin(9) = "eastward_wind"
-  self%varin(10) = "northward_wind"
-  self%varin(11) = "landsea_mask"
-  print *, 'varin'
-  print *, self%varin
-  print *, 'varout'
-  print *, self%varout
+  self%varin(5) = var_ps
+  self%varin(6) = var_prs
+  self%varin(7) = var_ts
+  self%varin(8) = var_q
+  self%varin(9) = var_u 
+  self%varin(10) = var_v 
+  self%varin(11) = var_sfc_lfrac
 
 end subroutine ufo_atmsfcinterp_setup
 
@@ -96,17 +88,34 @@ subroutine ufo_atmsfcinterp_simobs(self, geovals, hofx, obss)
   type(ufo_geovals),  intent(in)    :: geovals
   real(c_double),     intent(inout) :: hofx(:)
   type(c_ptr), value, intent(in)    :: obss
-  type(ufo_geoval), pointer :: phi, hgt, profile
+  type(ufo_geoval), pointer :: phi, hgt, tsfc, roughlen, psfc, prs, &
+                               tsen, q, u, v, landmask, &
+                               profile
   integer :: nlocs, ivar, iobs
   real(kind_real), allocatable :: obselev(:), obshgt(:)
   character(len=MAXVARLEN) :: geovar
 
 
+  ! to compute the value near the surface we are going to use
+  ! similarity theory which requires a number of near surface parameters
+  ! that need to be grabbed from GeoVaLs regardless of the observation type
+
   ! for low altitudes, we can assume: phi = z
-  ! get geopotential height profile
+  ! grabbing geopotential height profile and surface value,
+  ! surface temperature, surface roughness length
+  ! surface and profile air pressure
+  ! profiles of tsen, q, u, v, and surface land fraction
   call ufo_geovals_get_var(geovals, var_z, phi)
-  ! get surface geopotential height
-  !call ufo_geovals_get_var(geovals, var_sfc_z, hgt)
+  call ufo_geovals_get_var(geovals, var_sfc_z, hgt)
+  call ufo_geovals_get_var(geovals, var_sfc_t, tsfc)
+  call ufo_geovals_get_var(geovals, var_sfc_rough, roughlen)
+  call ufo_geovals_get_var(geovals, var_ps, psfc)
+  call ufo_geovals_get_var(geovals, var_prs, prs)
+  call ufo_geovals_get_var(geovals, var_ts, tsen)
+  call ufo_geovals_get_var(geovals, var_q, q)
+  call ufo_geovals_get_var(geovals, var_u, u)
+  call ufo_geovals_get_var(geovals, var_v, v)
+  call ufo_geovals_get_var(geovals, var_sfc_lfrac, landmask)
 
   ! get number of obs
   nlocs = obsspace_get_nlocs(obss)
