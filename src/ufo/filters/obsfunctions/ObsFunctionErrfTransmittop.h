63ccae5df090d2dedbeef9fba5ca34a6cc4510b2
Emily Liu <emily.liu@noaa.gov>
2020-01-15 06:21:57 +0000

    Add three obs functions, related yaml files, updated CMakeLists, and input files for IASI QC

---
+++ src/ufo/filters/obsfunctions/ObsFunctionErrfTransmittop.h
@@ -0,0 +1,39 @@
+/*
+ * (C) Copyright 2019 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#ifndef UFO_FILTERS_OBSFUNCTIONS_OBSFUNCTIONERRFTRANSMITTOP_H_
+#define UFO_FILTERS_OBSFUNCTIONS_OBSFUNCTIONERRFTRANSMITTOP_H_
+
+#include <vector>
+
+#include "ufo/filters/ObsFilterData.h"
+#include "ufo/filters/obsfunctions/ObsFunctionBase.h"
+#include "ufo/filters/Variables.h"
+
+namespace ufo {
+
+// -----------------------------------------------------------------------------
+
+class ObsFunctionErrfTransmittop : public ObsFunctionBase {
+ public:
+  explicit ObsFunctionErrfTransmittop(const eckit::LocalConfiguration);
+  ~ObsFunctionErrfTransmittop();
+
+  void compute(const ObsFilterData &,
+               ioda::ObsDataVector<float> &) const;
+  const ufo::Variables & requiredVariables() const;
+ private:
+  ufo::Variables invars_;
+  std::vector<int> channels_;
+  const eckit::LocalConfiguration conf_;
+};
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
+
+#endif  // UFO_FILTERS_OBSFUNCTIONS_OBSFUNCTIONERRFTRANSMITTOP_H_
