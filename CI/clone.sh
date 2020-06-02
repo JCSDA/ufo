a1a5c034ae37643d7cab9a9144ce779bc26bd4e2
Maryam Abdi <maryamao@ucar.edu>
2020-06-02 10:56:19 -0600

    changing ecmwf branch to release-stable in CI (#1080)
    * CI clean up and chang fckit and atlas to release-stable
    
    * bugfix
    
    * check similar branch name for atlas and fckit
    
    * remove buildspec from home dir
    
    * bugfix
    
    * bugfix
    
    * remove clone in project setting of travis
    
    * remove release-stable from cmakelists bundle
    
    * use :revert
    
    * try cloning a public repo
    
    * check atlas and fckit branch
    
    * trigger tests
    
    * debug
    
    * more debug
    
    * pass compiler to cmake
    
    * check PATH
    
    * bugfix!
    
    * debug with revert
    
    * use new gnu container
    
    * update docker for travis
    
    * add github token to all repos
    
    * disbale codecov in travis-ci
    
    * clean up

--- CI/clone.sh
+++ CI/clone.sh
@@ -9,6 +9,7 @@ repo_name=$3
 branch_name=$4
 save_name=$5
 save_dir=$6
+branch_name_default=$7
 
 echo "==============================================================================="
 echo "Clone " $repo_name
@@ -18,12 +19,12 @@ git ls-remote --heads --exit-code https://$git_user:$git_token@github.com/jcsda/
 exit_code=$?
 
 if test "${exit_code}" == "0"; then
-  echo ${branch_name} " branch found"
   branch_name_clone=${branch_name}
+  echo ${branch_name} " branch found"
 else
+  branch_name_clone=${branch_name_default}
   echo ${branch_name} " branch does not exist"
-  echo "clone develop"
-  branch_name_clone="develop"
+  echo "clone " ${branch_name_clone}
 fi
 
 git clone -b $branch_name_clone https://$git_user:$git_token@github.com/jcsda/$repo_name $save_dir/$save_name
