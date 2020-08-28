a692805c5a9edd843a1bc1e1e8e36d482c771912
JJ Guerrette <guerrett@ucar.edu>
2020-08-28 09:24:18 -0600

    Symmetric Cloud Impact for IR instruments (#1234)
    * Add symmetric cloud impact error inflation and single Okamoto SCI function
    
    * Make ramp function more generic, builds successfully
    
    * Fix coding norms
    
    * Add quadratic piece-wise function and move parameters to class header
    
    * Simplify config variable names, remove ObsFunction ptr
    
    * Small clean up
    
    * Pretty pictures
    
    * Account for difference between channels specified in Variable and ObsFunction (options)
    
    may revert these changes later if Variable gets refactored
    
    * Implement Parameters subclass.  Still needs testing.
    
    * Fix bugs in Parameters for ErrfRampRadiance
    
    * Remove ObsFunction prefixes and add Parameters to OkamotoSCI
    
    * Add Parameters to ObsErrorQuadRadiance
    
    plus minor cleanups/commenting/TODO's
    
    * Temporarily do not use bias
    
    Note: eventually need to check if bias is present, then conditionally use it
    
    * Clean up SCI function
    
    * Add RequiredParameter distinctions
    
    Enable Ramp and Quad functions to work with/without channel-specific x-variables
    
    * Coding norms
    
    * More complete example for CLWRetMean
    
    * Improve configuration examples
    
    * Rename ObsFunctions and improve re-usability
    
    * Replace ObsFunctionObsErrorMean with ObsErrorModelRampRad
    
    * Remove unused files
    
    * Fix comment
    
    * Some updates to keep up with feature/ramprad
    
    * Improve Doxygen documentation and rename to ObsErrorModelRamp
    
    * Clean up Doxygen comments
    
    * Update copyright dates
    
    * Duplicate Ramp formatting/Parameters/naming in Quad
    
    * Change vector assign to fill
    
    * Include rank in default Thinning seed
    
    * Revert seed changes
    
    * Use global index to access subset of random values
    
    * Fix gnlocs bug
    
    * Add citation for Okamoto et al.
    
    * Add SymmCldImpact test
    
    * Add SymmCldImpact test data files
    
    * Uncomment FPE check
    
    * Save unused ObsErrorModelQuad for future PR
    
    * Use obs file from IODA tarball
    
    * trigger tests
    
    Co-authored-by: Stephen Herbener <stephen.herbener@gmail.com>

---
+++ src/ufo/filters/obsfunctions/SymmCldImpactIR.cc
@@ -0,0 +1,98 @@
+/*
+ * (C) Copyright 2020 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "ufo/filters/obsfunctions/SymmCldImpactIR.h"
+
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "ioda/ObsDataVector.h"
+#include "oops/util/IntSetParser.h"
+#include "oops/util/missingValues.h"
+#include "ufo/filters/Variable.h"
+#include "ufo/utils/Constants.h"
+
+namespace ufo {
+
+static ObsFunctionMaker<SymmCldImpactIR> makerSCIIR_("SymmCldImpactIR");
+
+// -----------------------------------------------------------------------------
+
+SymmCldImpactIR::SymmCldImpactIR(const eckit::LocalConfiguration config)
+  : invars_(), channels_() {
+  // Initialize options
+  options_.deserialize(config);
+
+  // Get channels from options
+  std::string chlist = options_.chlist;
+  std::set<int> channelset = oops::parseIntSet(chlist);
+  std::copy(channelset.begin(), channelset.end(), std::back_inserter(channels_));
+
+  // Include required variables from ObsDiag
+  invars_ += Variable("brightness_temperature_assuming_clear_sky@ObsDiag", channels_);
+}
+
+// -----------------------------------------------------------------------------
+
+SymmCldImpactIR::~SymmCldImpactIR() {}
+
+// -----------------------------------------------------------------------------
+
+void SymmCldImpactIR::compute(const ObsFilterData & in,
+                                    ioda::ObsDataVector<float> & SCI) const {
+  const float missing = util::missingValue(missing);
+
+  // Get dimensions
+  size_t nlocs = in.nlocs();
+
+  // Allocate vectors common across channels
+  std::vector<float> clr(nlocs);
+  std::vector<float> bak(nlocs);
+  std::vector<float> obs(nlocs);
+  std::vector<float> bias(nlocs);
+
+  float Cmod, Cobs;
+
+  for (size_t ich = 0; ich < SCI.nvars(); ++ich) {
+    // Get channel-specific clr, bak, obs, and bias
+    in.get(Variable("brightness_temperature_assuming_clear_sky@ObsDiag", channels_)[ich], clr);
+    in.get(Variable("brightness_temperature@HofX", channels_)[ich], bak);
+    in.get(Variable("brightness_temperature@ObsValue", channels_)[ich], obs);
+    if (in.has(Variable("brightness_temperature@ObsBias", channels_)[ich])) {
+      in.get(Variable("brightness_temperature@ObsBias", channels_)[ich], bias);
+    } else {
+      std::fill(bias.begin(), bias.end(), 0.0f);
+    }
+    for (size_t iloc = 0; iloc < nlocs; ++iloc) {
+      if (clr[iloc] != missing && bak[iloc] != missing &&
+          obs[iloc] != missing && bias[iloc] != missing) {
+        // Temporarily account for ZERO clear-sky BT output from CRTM
+        // TODO(JJG): change CRTM clear-sky behavior
+        if (clr[iloc] > -1.0f && clr[iloc] < 1.0f) clr[iloc] = bak[iloc];
+
+        Cmod = abs(clr[iloc] - bak[iloc]);
+        Cobs = abs(clr[iloc] - obs[iloc] + bias[iloc]);
+        SCI[ich][iloc] = 0.5f * (Cmod + Cobs);
+      } else {
+        SCI[ich][iloc] = missing;
+      }
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+const ufo::Variables & SymmCldImpactIR::requiredVariables() const {
+  return invars_;
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
