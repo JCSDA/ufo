0534d485dd239a9415d974cd32c66e9319757ad3
mer-a-o <maryamao@ucar.edu>
2020-10-26 17:54:05 -0600

    make checksum optional

--- test/ufo_ioda_data_downloader.py
+++ test/ufo_ioda_data_downloader.py
@@ -12,6 +12,7 @@ download_file_name = sys.argv[1]
 testfiles_name = sys.argv[2]
 testfiles_path = sys.argv[3]
 download_base_url = sys.argv[4]
+md5check = sys.argv[5]
 
 def DownloadUntar(download_base_url, download_file_name, testfiles_path, testfiles_name):
   urllib.request.urlretrieve( download_base_url+"/"+download_file_name+".md5", testfiles_path+"/"+testfiles_name+".md5")
@@ -20,26 +21,37 @@ def DownloadUntar(download_base_url, download_file_name, testfiles_path, testfil
   tar_file.extractall(testfiles_path)
   tar_file.close()
 
-#  if .tar.gz and .tar.gz.md5 exist 
-#  then download s3 md5
-#  and compare with local md5
-if os.path.isfile(testfiles_path+"/"+testfiles_name) and os.path.isfile(testfiles_path+"/"+testfiles_name+".md5") :
-  print("local files found")
 
-  #  dl md5 save it as *.md5.dl
-  urllib.request.urlretrieve( download_base_url+"/"+download_file_name+".md5", testfiles_path+"/"+testfiles_name+".md5.dl")
+if md5check == "1" :
+  #  if .tar.gz and .tar.gz.md5 exist
+  #  then download s3 md5
+  #  and compare with local md5
+  print ("checking md5sum")
+  if os.path.isfile(testfiles_path+"/"+testfiles_name) and os.path.isfile(testfiles_path+"/"+testfiles_name+".md5") :
+    print("local files found")
 
-  #  compare *md5.dl with md5 local
-  with open(testfiles_path+"/"+testfiles_name+".md5", 'r') as f:
-    md5_local = f.read()
-  with open(testfiles_path+"/"+testfiles_name+".md5.dl", 'r') as f:
-    md5_dl = f.read()
-  if md5_local == md5_dl :
-    print("no update in dataset")
+    #  dl md5 save it as *.md5.dl
+    urllib.request.urlretrieve( download_base_url+"/"+download_file_name+".md5", testfiles_path+"/"+testfiles_name+".md5.dl")
+
+    #  compare *md5.dl with md5 local
+    with open(testfiles_path+"/"+testfiles_name+".md5", 'r') as f:
+      md5_local = f.read()
+    with open(testfiles_path+"/"+testfiles_name+".md5.dl", 'r') as f:
+      md5_dl = f.read()
+    if md5_local == md5_dl :
+      print("no update in dataset")
+    else:
+      print("update found; download new dataset")
+      DownloadUntar(download_base_url, download_file_name, testfiles_path, testfiles_name)
   else:
-    print("update found; download new dataset")
+    print("local file not found; download from S3")
+    print("downloading "+ download_base_url+"/"+download_file_name)
     DownloadUntar(download_base_url, download_file_name, testfiles_path, testfiles_name)
+
 else:
-  print("local file not found; download from S3")
-  print("downloading "+ download_base_url+"/"+download_file_name)
-  DownloadUntar(download_base_url, download_file_name, testfiles_path, testfiles_name)
+  # downloading release data from DASH
+  if os.path.isfile(testfiles_path+"/"+testfiles_name):
+    print("local RELEASE file found")
+  else:
+    print ("dowloading RELEASE data from "+download_base_url+"/"+download_file_name)
+    DownloadUntar(download_base_url, download_file_name, testfiles_path, testfiles_name)
