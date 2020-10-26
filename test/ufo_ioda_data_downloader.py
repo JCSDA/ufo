661bad47a23c3a06fae92d9f0726bca224911f15
shlyaeva <shlyaeva@gmail.com>
2020-10-26 06:41:56 -0600

    add not-tested option of downloading from ftp + refactor a bit

--- test/ufo_ioda_data_downloader.py
+++ test/ufo_ioda_data_downloader.py
@@ -8,17 +8,14 @@ import urllib.request
 
 bucket_name = "jedi-test-files"
 
-repository_name = sys.argv[1]
+download_file_name = sys.argv[1]
 testfiles_name = sys.argv[2]
-branch_name = sys.argv[3]
-testfiles_path = sys.argv[4]
-download_base_url = sys.argv[5]
+testfiles_path = sys.argv[3]
+download_base_url = sys.argv[4]
 
-s3_file_name = repository_name+"/"+branch_name+"/"+testfiles_name
-
-def DownloadUntar(download_base_url, s3_file_name, testfiles_path, testfiles_name):
-  urllib.request.urlretrieve( download_base_url+"/"+s3_file_name+".md5", testfiles_path+"/"+testfiles_name+".md5")
-  urllib.request.urlretrieve( download_base_url+"/"+s3_file_name, testfiles_path+"/"+testfiles_name)
+def DownloadUntar(download_base_url, download_file_name, testfiles_path, testfiles_name):
+  urllib.request.urlretrieve( download_base_url+"/"+download_file_name+".md5", testfiles_path+"/"+testfiles_name+".md5")
+  urllib.request.urlretrieve( download_base_url+"/"+download_file_name, testfiles_path+"/"+testfiles_name)
   tar_file = tarfile.open(testfiles_path+"/"+testfiles_name)
   tar_file.extractall(testfiles_path)
   tar_file.close()
@@ -30,7 +27,7 @@ if os.path.isfile(testfiles_path+"/"+testfiles_name) and os.path.isfile(testfile
   print("local files found")
 
   #  dl md5 save it as *.md5.dl
-  urllib.request.urlretrieve( download_base_url+"/"+s3_file_name+".md5", testfiles_path+"/"+testfiles_name+".md5.dl")
+  urllib.request.urlretrieve( download_base_url+"/"+download_file_name+".md5", testfiles_path+"/"+testfiles_name+".md5.dl")
 
   #  compare *md5.dl with md5 local
   with open(testfiles_path+"/"+testfiles_name+".md5", 'r') as f:
@@ -41,8 +38,8 @@ if os.path.isfile(testfiles_path+"/"+testfiles_name) and os.path.isfile(testfile
     print("no update in dataset")
   else:
     print("update found; download new dataset")
-    DownloadUntar(download_base_url, s3_file_name, testfiles_path, testfiles_name)
+    DownloadUntar(download_base_url, download_file_name, testfiles_path, testfiles_name)
 else:
   print("local file not found; download from S3")
-  print("downloading "+ download_base_url+"/"+s3_file_name)
-  DownloadUntar(download_base_url, s3_file_name, testfiles_path, testfiles_name)
+  print("downloading "+ download_base_url+"/"+download_file_name)
+  DownloadUntar(download_base_url, download_file_name, testfiles_path, testfiles_name)
