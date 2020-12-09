e076d84818b6a3fb973f1091c50a78f13e601208
James Cotton <64855970+james-cotton@users.noreply.github.com>
2020-12-09 15:36:19 +0000

    Feature/met office amv operator test (#357)
    * Add satwind operator test for Met Office
    
    * Update input file
    
    * Change test date
    
    * tweak filenames for test
    
    * Update test file locations
    
    Co-authored-by: Ryan Honeyager <honeyage@ucar.edu>

--- src/ufo/atmvertinterp/ufo_atmvertinterp_mod.F90
+++ src/ufo/atmvertinterp/ufo_atmvertinterp_mod.F90
@@ -45,7 +45,10 @@ subroutine atmvertinterp_setup_(self, grid_conf)
   if( grid_conf%has("vertical coordinate") ) then
       call grid_conf%get_or_die("vertical coordinate",coord_name)
       self%v_coord = coord_name
-      if( trim(self%v_coord) .eq. var_prs ) self%use_ln = .true.
+      if( (trim(self%v_coord) .eq. var_prs) .or. &
+          (trim(self%v_coord) .eq. var_prsi) ) then 
+        self%use_ln = .true.
+      endif
   else  ! default
       self%v_coord = var_prs
       self%use_ln  = .true.
