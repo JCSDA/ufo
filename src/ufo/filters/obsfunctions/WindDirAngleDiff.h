8c34dab92fb48da3a0bb1bf4b473519d5aa0eedd
Greg Thompson <63315545+gthompsnJCSDA@users.noreply.github.com>
2020-12-15 10:50:27 -0700

    A new WindDirAngleDiff@ObsFunction to calculate obs-model wind direction difference used in wind QC (#483)
    * initial commit for a wind-direction angle difference calculation (obs-model)
    
    * add a new unit_tests subdir; add a new bounds check of new WindDirAngleDiff@ObsFunction
    
    * fix dir path to new ctest and value of passed benchmark
    
    * add spaces for coding norms
    
    * per reviewers suggestions, changed a number of items
    
    Co-authored-by: Greg Thompson <gthompsn@ucar.edu>

---
+++ src/ufo/filters/obsfunctions/WindDirAngleDiff.h
@@ -0,0 +1,73 @@
+/*
+ * (C) Copyright 2020 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#ifndef UFO_FILTERS_OBSFUNCTIONS_WINDDIRANGLEDIFF_H_
+#define UFO_FILTERS_OBSFUNCTIONS_WINDDIRANGLEDIFF_H_
+
+#include <string>
+
+#include "oops/util/parameters/Parameter.h"
+
+#include "ufo/filters/ObsFilterData.h"
+#include "ufo/filters/obsfunctions/ObsFunctionBase.h"
+#include "ufo/filters/Variables.h"
+
+namespace ufo {
+
+///
+/// \brief An optional parameter to override the source of HofX wind components,
+///        and an optional parameter for minimum wind components (default=0.5 m/s).
+///
+class WindDirAngleDiffParameters : public oops::Parameters {
+  OOPS_CONCRETE_PARAMETERS(WindDirAngleDiffParameters, Parameters)
+
+ public:
+  /// Name of the HofX group used to replace the default group (default is HofX)
+  oops::Parameter<std::string> test_hofx{"test_hofx", "HofX", this};
+  oops::Parameter<float> minimum_uv{"minimum_uv", 0.5, this};
+};
+
+// -----------------------------------------------------------------------------
+
+/// \brief Compute the wind direction angle difference between observation and model.
+///        For application in SatWinds QC, a difference larger than 50 degrees is
+///        typically used to reject data.  This threshold can be used as the max_value
+///        in a Bounds Check filter.
+///
+/// ~~~
+///
+/// ### Sample YAML configuration
+///     - filter: Bounds Check
+///       filter variables:
+///       - name: eastward_wind
+///       - name: northward_wind
+///       test variables:
+///       - name: WindDirAngleDiff@ObsFunction
+///         options:
+///           test_hofx: GsiHofX
+///       maxvalue: 50
+///
+class WindDirAngleDiff : public ObsFunctionBase {
+ public:
+  static const std::string classname() {return "WindDirAngleDiff";}
+
+  explicit WindDirAngleDiff(const eckit::LocalConfiguration &
+                                 = eckit::LocalConfiguration());
+  ~WindDirAngleDiff();
+
+  void compute(const ObsFilterData &, ioda::ObsDataVector<float> &) const;
+  const ufo::Variables & requiredVariables() const;
+ private:
+  ufo::Variables invars_;
+  WindDirAngleDiffParameters options_;
+};
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
+
+#endif  // UFO_FILTERS_OBSFUNCTIONS_WINDDIRANGLEDIFF_H_
