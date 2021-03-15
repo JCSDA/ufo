2b9d2f6c51033390ff1cb0ae58fa52d220893b8b
Benjamin Ruston <42614642+rusbec@users.noreply.github.com>
2021-03-15 12:09:40 -0700

    Feature/ground based gnss ropp (#902)
    * add files for ground based GNSS simulation using the ROPP
    
    * add changes to ground based GNSS CMakeLists to allow building of subdirectory for ROPP forward operator
    
    * add ctest for ground based GNSS using ROPP
    
    * add ground based GNSS with ROPP ctest execution block
    
    * try to get correct name of test name in operator factory
    
    * add correction to include ground based GNSS with ROPP files
    
    * fix some references to radio occultation in the ground based GNSS forward operator
    
    * modify references from bending angle to refractivity
    
    * add changes to state and observation vector to be passed into ROPP
    
    * add necessary pieces to compute forward ztd and add debug check against observation
    
    * fix coding norm issues in header files
    
    * add compute model_ztd to hofx and change tolerance of ctest HofX versus HofX
    
    * piddly little change to beautify the verbose debug printing in ROPP based ground based GNSS
    
    * add error trap if integral of refractivity to get ZTD returns zero
    
    * put in the removal of GNSS RO related variables to the simulation stub
    
    * remove some unused variable and fix conditional bug introduced
    
    * initialize the hofx in the stub to missing
    
    Co-authored-by: rusbec <ben.ruston@nrlmry.navy.mil>
    Co-authored-by: Ryan Honeyager <honeyage@ucar.edu>

---
+++ src/ufo/groundgnss/RefROPP1D/ObsGnssGBRefROPP1D.cc
@@ -0,0 +1,63 @@
+/*
+ * (C) Copyright 2017-2018 UCAR
+ * 
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
+ */
+
+#include "ufo/groundgnss/RefROPP1D/ObsGnssGBRefROPP1D.h"
+
+#include <ostream>
+#include <string>
+#include <vector>
+
+#include "ioda/ObsVector.h"
+
+#include "oops/base/Variables.h"
+#include "oops/util/Logger.h"
+
+#include "ufo/GeoVaLs.h"
+#include "ufo/ObsDiagnostics.h"
+
+namespace ufo {
+
+// -----------------------------------------------------------------------------
+static ObsOperatorMaker<ObsGnssGBRefROPP1D> makerGnssGBRefROPP1D_("GnssGBRefROPP1D");
+// -----------------------------------------------------------------------------
+
+ObsGnssGBRefROPP1D::ObsGnssGBRefROPP1D(const ioda::ObsSpace & odb,
+                                       const eckit::Configuration & config)
+  : ObsOperatorBase(odb, config), keyOperGnssGBRefROPP1D_(0), odb_(odb), varin_()
+{
+  const std::vector<std::string> vv{"air_temperature", "specific_humidity", "air_pressure",
+                                    "geopotential_height", "surface_altitude"};
+  varin_.reset(new oops::Variables(vv));
+
+  ufo_gnssgb_refropp1d_setup_f90(keyOperGnssGBRefROPP1D_, config);
+  oops::Log::trace() << "ObsGnssGBRefROPP1D created." << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+ObsGnssGBRefROPP1D::~ObsGnssGBRefROPP1D() {
+  ufo_gnssgb_refropp1d_delete_f90(keyOperGnssGBRefROPP1D_);
+  oops::Log::trace() << "ObsGnssGBRefROPP1D destructed" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+void ObsGnssGBRefROPP1D::simulateObs(const GeoVaLs & gom, ioda::ObsVector & ovec,
+                                     ObsDiagnostics &) const {
+  ufo_gnssgb_refropp1d_simobs_f90(keyOperGnssGBRefROPP1D_, gom.toFortran(), odb_,
+                                  ovec.size(), ovec.toFortran());
+}
+
+// -----------------------------------------------------------------------------
+
+void ObsGnssGBRefROPP1D::print(std::ostream & os) const {
+  os << "ObsGnssGBRefROPP1D::print not implemented";
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
