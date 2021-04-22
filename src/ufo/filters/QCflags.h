257ec9658c320f3dd5fbcbaab8e55b988f07a7af
Yannick Trémolet <30638944+ytremolet@users.noreply.github.com>
2021-04-22 14:44:50 -0600

    Changed QC flags values (#1088)

--- src/ufo/filters/QCflags.h
+++ src/ufo/filters/QCflags.h
@@ -11,25 +11,30 @@
 namespace ufo {
 
 namespace QCflags {
-  constexpr int pass    = 0;   // we like that one!
-  constexpr int missing = 1;   // missing values prevent use of observation
-  constexpr int preQC   = 2;   // observation rejected by pre-processing
-  constexpr int bounds  = 3;   // observation value out of bounds
-  constexpr int domain  = 4;   // observation not within domain of use
-  constexpr int black   = 5;   // observation black listed
-  constexpr int Hfailed = 6;   // H(x) computation failed
-  constexpr int thinned = 7;   // observation removed due to thinning
-  constexpr int diffref = 8;   // metadata too far from reference
-  constexpr int clw     = 9;   // observation removed due to cloud field
-  constexpr int fguess  = 10;  // observation too far from guess
-  constexpr int seaice  = 11;  // observation based sea ice detection, also flags land points
-  constexpr int track   = 12;  // observation removed as inconsistent with the rest of track
-  constexpr int buddy   = 13;  // observation rejected by the buddy check
-  constexpr int derivative = 14;  // observation removed due to metadata derivative value
-  constexpr int profile = 15;  // observation rejected by at least one profile QC check
-  constexpr int onedvar = 16;  // observation failed to converge in 1dvar check
-  constexpr int bayesianQC = 17;  // observation failed due to Bayesian background check
-  constexpr int modelobthresh = 18;  // observation failed modelob threshold check
+  constexpr int pass    = 0;  // we like that one!
+  constexpr int passive = 1;  // H(x) is computed (for monitoring, BC...) but obs not assimilated
+// Single digit values reserved for DA use.
+// For now only 0, 1 and >1 are used but keeping space for other potential use cases.
+
+// Actual rejection flags
+  constexpr int missing = 10;  // missing values prevent use of observation
+  constexpr int preQC   = 11;  // observation rejected by pre-processing
+  constexpr int bounds  = 12;  // observation value out of bounds
+  constexpr int domain  = 13;  // observation not within domain of use
+  constexpr int black   = 14;  // observation black listed
+  constexpr int Hfailed = 15;  // H(x) computation failed
+  constexpr int thinned = 16;  // observation removed due to thinning
+  constexpr int diffref = 17;  // metadata too far from reference
+  constexpr int clw     = 18;  // observation removed due to cloud field
+  constexpr int fguess  = 19;  // observation too far from guess
+  constexpr int seaice  = 20;  // observation based sea ice detection, also flags land points
+  constexpr int track   = 21;  // observation removed as inconsistent with the rest of track
+  constexpr int buddy   = 22;  // observation rejected by the buddy check
+  constexpr int derivative = 23;  // observation removed due to metadata derivative value
+  constexpr int profile = 24;  // observation rejected by at least one profile QC check
+  constexpr int onedvar = 25;  // observation failed to converge in 1dvar check
+  constexpr int bayesianQC = 26;  // observation failed due to Bayesian background check
+  constexpr int modelobthresh = 27;  // observation failed modelob threshold check
 };  // namespace QCflags
 
 }  // namespace ufo
