1787974f888253d23d9234cb35554f5547e39deb
Michael Cooke <48374999+mikecooke77@users.noreply.github.com>
2021-07-09 20:13:26 +0100

    Add channel support for linear combination obs function (#1303)
    * Initial attemp
    
    * Its working but needs tidying
    
    * Tidy up and add multichannel test
    
    * Changes for coding norms
    
    * Improve the testing
    
    * remove logging
    
    * Add comments and initialize to zero
    
    * Remove index calculation as its not needed
    
    * Change the way the variables are read in
    
    * Changes needed
    
    * Test case computing a linear combination of obs functions (#1306)
    
    * Added a test case verifying it is possible to calculate a linear combination of ObsFunctions.
    
    * Added a comment.
    
    * Unified yaml file
    
    * Trigger build
    
    * Updated test data
    
    Co-authored-by: Wojciech Śmigaj <wojciech.smigaj@metoffice.gov.uk>
    Co-authored-by: Ryan Honeyager <honeyage@ucar.edu>

--- src/ufo/filters/obsfunctions/ObsFunctionLinearCombination.cc
+++ src/ufo/filters/obsfunctions/ObsFunctionLinearCombination.cc
@@ -7,9 +7,12 @@
 
 #include "ufo/filters/obsfunctions/ObsFunctionLinearCombination.h"
 
+#include <algorithm>
+#include <set>
 #include <vector>
 
 #include "ioda/ObsDataVector.h"
+#include "oops/util/IntSetParser.h"
 #include "oops/util/missingValues.h"
 
 namespace ufo {
@@ -22,15 +25,11 @@ static ObsFunctionMaker<LinearCombination>
 LinearCombination::LinearCombination(const eckit::LocalConfiguration & conf)
   : invars_() {
   // Check options
-  options_.deserialize(conf);
+  options_.validateAndDeserialize(conf);
 
-  // number of input variables
-  int nv = options_.variables.value().size();
-
-  // get variable informations
-  std::vector<ufo::Variable> variables = options_.variables.value();
-  for (size_t ii = 0; ii < nv; ++ii) {
-    invars_ += variables[ii];
+  // Create variable and add to invars_
+  for (const Variable & var : options_.variables.value()) {
+    invars_ += var;
   }
 }
 
@@ -46,27 +45,31 @@ void LinearCombination::compute(const ObsFilterData & in,
   const size_t nlocs = in.nlocs();
 
   // number of input variables
-  int nv = invars_.size();
+  const size_t nv = invars_.size();
 
   // get coefs for linear combination
   const std::vector<float> coefs = options_.coefs.value();
 
-  // sanity check
-  ASSERT(coefs.size() == invars_.size() );
+  // sanity checks / initialize
+  ASSERT(coefs.size() == nv);
+  out.zero();
 
-  // compute linear conbination of input variables
+  // compute linear combination of input variables
   const float missing = util::missingValue(missing);
-  std::vector<float> varin(nlocs);
-  for (size_t ii = 0; ii < nv; ++ii) {
-    in.get(invars_[ii], varin);
-    for (size_t jj = 0; jj < nlocs; ++jj) {
-      if ( varin[jj] == missing || out[0][jj] == missing ) {
-        out[0][jj] = missing;
-      } else {
-        out[0][jj] += coefs[ii]*varin[jj];
-      }
-    }
-  }
+  for (size_t ivar = 0; ivar < nv; ++ivar) {
+    ioda::ObsDataVector<float> varin(in.obsspace(), invars_[ivar].toOopsVariables());
+    in.get(invars_[ivar], varin);
+    ASSERT(varin.nvars() == out.nvars());
+    for (size_t iloc = 0; iloc < nlocs; ++iloc) {
+      for (size_t ichan = 0; ichan < out.nvars(); ++ichan) {
+        if ( varin[ichan][iloc] == missing || out[ichan][iloc] == missing ) {
+          out[ichan][iloc] = missing;
+        } else {
+          out[ichan][iloc] += coefs[ivar] * varin[ichan][iloc];
+        }
+      }  // ichan
+    }  // nlocs
+  }  // nvars
 }
 
 // -----------------------------------------------------------------------------
