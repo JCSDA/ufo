11a4d168778c5c161ba6f679d4a40d7cb4bf6a4f
Anna Shlyaeva <shlyaeva@gmail.com>
2021-09-07 14:54:09 -0600

    Update obs error correlations I/O (#1480)
    * read obs error correlations only for specified variables
    
    * add test for cross-channel correlations + change the print
    
    * update comments
    
    Co-authored-by: Ryan Honeyager <honeyage@ucar.edu>

--- src/ufo/utils/IodaGroupIndices.h
+++ src/ufo/utils/IodaGroupIndices.h
@@ -24,16 +24,21 @@ namespace ufo {
 /// Return the vector of indices of all elements in the range
 /// [\p elements_to_look_for_begin, \p elements_to_look_for_end) in the \p varname ioda::Variable
 /// from \p obsgroup ioda::ObsGroup, in the same order that the former are in.
-/// Throws an exception if at least one of elements looked for is missing.
+/// If \p throwexception is set, throws an exception if at least one of the elements looked for is
+/// missing. Otherwise sets index as -1 for the missing elements.
 std::vector<int> getRequiredVariableIndices(const ioda::ObsGroup &obsgroup,
                  const std::string &varname,
                  typename std::vector<std::string>::const_iterator elements_to_look_for_begin,
-                 typename std::vector<std::string>::const_iterator elements_to_look_for_end);
+                 typename std::vector<std::string>::const_iterator elements_to_look_for_end,
+                 bool throwexception = true);
 
 /// Return the vector of indices of variables or channels \p vars_to_look_for in the
-/// "variables" or "channels" ioda::Variable from \p obsgroup ioda::ObsGroup
+/// "variables" or "channels" ioda::Variable from \p obsgroup ioda::ObsGroup.
+/// If \p throwexception is set, throws an exception if at least one of the elements looked for is
+/// missing. Otherwise sets index as -1 for the missing elements.
 std::vector<int> getRequiredVarOrChannelIndices(const ioda::ObsGroup &obsgroup,
-                                                const oops::Variables &vars_to_look_for);
+                                                const oops::Variables &vars_to_look_for,
+                                                bool throwexception = true);
 
 }  // namespace ufo
 
