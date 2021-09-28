22bbf1bf0a918d269481481e71ac240c17486cb3
Francois Hebert <fmahebert@gmail.com>
2021-09-28 13:48:00 -0700

    Refactor Locations class to store data in ioda::ObsGroup (#1496)
    * Refactor Locations to store data in ioda::ObsGroup
    
    * Update Locations copyright year
    
    * Set ObsGroup float_params to handle missing values
    
    * Restart CI
    
    Co-authored-by: Francois Hebert <hebert@ucar.edu>
    Co-authored-by: Benjamin Johnson <33431190+BenjaminTJohnson@users.noreply.github.com>
    Co-authored-by: Stephen Herbener <32968781+srherbener@users.noreply.github.com>
    Co-authored-by: Ryan Honeyager <honeyage@ucar.edu>

--- src/ufo/Locations.cc
+++ src/ufo/Locations.cc
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2017-2020 UCAR
+ * (C) Copyright 2017-2021 UCAR
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -13,11 +13,14 @@
 #include "eckit/config/Configuration.h"
 #include "eckit/exception/Exceptions.h"
 
+#include "ioda/Engines/Factory.h"
+#include "ioda/Group.h"
 #include "ioda/ObsSpace.h"
 
 #include "oops/mpi/mpi.h"
 #include "oops/util/DateTime.h"
 #include "oops/util/Logger.h"
+#include "oops/util/missingValues.h"
 
 namespace ufo {
 
@@ -26,9 +29,25 @@ namespace ufo {
 Locations::Locations(const std::vector<float> & lons, const std::vector<float> & lats,
                      const std::vector<util::DateTime> & times,
                      std::shared_ptr<const ioda::Distribution> dist)
-  : dist_(std::move(dist)), lons_(lons), lats_(lats), times_(times) {
-  ASSERT(lons_.size() == lats_.size());
-  ASSERT(lats_.size() == times_.size());
+  : dist_(std::move(dist)), times_(std::move(times)) {
+  const size_t nlocs = times_.size();
+  ASSERT(nlocs == lons.size());
+  ASSERT(nlocs == lats.size());
+
+  initializeObsGroup(nlocs);
+
+  // Set float_params that add safety in case of missing values and optimize the
+  // potential future use of hdf5's "memory file" as the ObsGroup backend.
+  const float floatMissing = util::missingValue(floatMissing);
+  ioda::VariableCreationParameters float_params;
+  float_params.chunk = true;
+  float_params.compressWithGZIP();
+  float_params.setFillValue<float>(floatMissing);
+
+  const ioda::Variable nlocsVar = og_.vars["nlocs"];
+  og_.vars.createWithScales<float>("longitude", {nlocsVar}, float_params).write(lons);
+  og_.vars.createWithScales<float>("latitude", {nlocsVar}, float_params).write(lats);
+
   oops::Log::trace() << "ufo::Locations::Locations done" << std::endl;
 }
 
@@ -60,24 +79,60 @@ Locations::Locations(const eckit::Configuration & conf,
   ioda::ObsTopLevelParameters obsparams;
   obsparams.validateAndDeserialize(obsconf);
 
-  ioda::ObsSpace obspace(obsparams, comm, bgn, end, oops::mpi::myself());
+  const ioda::ObsSpace obspace(obsparams, comm, bgn, end, oops::mpi::myself());
   const size_t nlocs = obspace.nlocs();
   dist_ = obspace.distribution();
 
-  lats_.resize(nlocs);
-  lons_.resize(nlocs);
-  times_.resize(nlocs);
+  initializeObsGroup(nlocs);
 
-  obspace.get_db("MetaData", "latitude", lats_);
-  obspace.get_db("MetaData", "longitude", lons_);
+  // Set float_params that add safety in case of missing values and optimize the
+  // potential future use of hdf5's "memory file" as the ObsGroup backend.
+  const float floatMissing = util::missingValue(floatMissing);
+  ioda::VariableCreationParameters float_params;
+  float_params.chunk = true;
+  float_params.compressWithGZIP();
+  float_params.setFillValue<float>(floatMissing);
+
+  const ioda::Variable nlocsVar = og_.vars["nlocs"];
+  std::vector<float> buffer(nlocs);
+  obspace.get_db("MetaData", "longitude", buffer);
+  og_.vars.createWithScales<float>("longitude", {nlocsVar}, float_params).write(buffer);
+  obspace.get_db("MetaData", "latitude", buffer);
+  og_.vars.createWithScales<float>("latitude", {nlocsVar}, float_params).write(buffer);
+
+  times_.resize(nlocs);
   obspace.get_db("MetaData", "datetime", times_);
 }
 
 // -------------------------------------------------------------------------------------------------
 Locations & Locations::operator+=(const Locations & other) {
-  lons_.insert(lons_.end(), other.lons_.begin(), other.lons_.end());
-  lats_.insert(lats_.end(), other.lats_.begin(), other.lats_.end());
+  // Resize ObsGroup to new total size
+  const ioda::Variable nlocsVar = og_.vars["nlocs"];
+  const size_t nlocs = nlocsVar.getDimensions().dimsCur[0];
+  const size_t other_nlocs = other.og_.vars["nlocs"].getDimensions().dimsCur[0];
+  const ioda::Dimensions_t total_nlocs = nlocs + other_nlocs;
+
+  og_.resize({std::make_pair(nlocsVar, total_nlocs)});
+
+  // Append variables from other's ObsGroup to end of local ObsGroup variables
+  const std::vector<ioda::Dimensions_t> start(1, nlocs);
+  const std::vector<ioda::Dimensions_t> other_start(1, 0);
+  const std::vector<ioda::Dimensions_t> counts(1, other_nlocs);
+
+  ioda::Selection feSelect;
+  feSelect.extent({total_nlocs}).select({ioda::SelectionOperator::SET, other_start, counts});
+  ioda::Selection beSelect;
+  beSelect.select({ioda::SelectionOperator::SET, start, counts});
+
+  std::vector<float> buffer(other_nlocs);
+  other.og_.vars["longitude"].read<float>(gsl::make_span(buffer));
+  og_.vars["longitude"].write<float>(buffer, feSelect, beSelect);
+
+  other.og_.vars["latitude"].read<float>(gsl::make_span(buffer));
+  og_.vars["latitude"].write<float>(buffer, feSelect, beSelect);
+
   times_.insert(times_.end(), other.times_.begin(), other.times_.end());
+
   return *this;
 }
 
@@ -94,7 +149,37 @@ std::vector<bool> Locations::isInTimeWindow(const util::DateTime & t1,
 // -------------------------------------------------------------------------------------------------
 
 size_t Locations::size() const {
-  return lats_.size();
+  return times_.size();
+}
+
+// -------------------------------------------------------------------------------------------------
+
+std::vector<float> Locations::lons() const {
+  ASSERT(og_.vars.exists("longitude"));
+  const size_t nlocs = size();
+  std::vector<float> lons(nlocs);
+  og_.vars["longitude"].read<float>(gsl::make_span(lons));
+  return lons;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+std::vector<float> Locations::lats() const {
+  ASSERT(og_.vars.exists("latitude"));
+  const size_t nlocs = size();
+  std::vector<float> lats(nlocs);
+  og_.vars["latitude"].read<float>(gsl::make_span(lats));
+  return lats;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void Locations::initializeObsGroup(const size_t nlocs) {
+  ioda::Engines::BackendCreationParameters params;
+  ioda::Group g = constructBackend(ioda::Engines::BackendNames::ObsStore, params);
+  const ioda::NewDimensionScales_t dimScales{{
+      ioda::NewDimensionScale<int>("nlocs", nlocs, ioda::Unlimited, nlocs)}};
+  og_ = ioda::ObsGroup::generate(g, dimScales);
 }
 
 // -------------------------------------------------------------------------------------------------
