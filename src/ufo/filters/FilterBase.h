8d13f22cf6aba68fc87af3d4e7fbae8244946c3d
Wojciech Śmigaj <wojciech.smigaj@metoffice.gov.uk>
2021-10-18 20:24:53 +0100

    Describe ways of handling configuration options in FilterBase subclasses (#1581)
    * Describe ways of handling configuration options in FilterBase subclasses.
    
    * Add parameter names.
    
    Co-authored-by: Ryan Honeyager <honeyage@ucar.edu>

--- src/ufo/filters/FilterBase.h
+++ src/ufo/filters/FilterBase.h
@@ -41,7 +41,28 @@ namespace ufo {
 ///
 /// Filters only need to implement the constructor and the applyFilter, print and qcFlag methods;
 /// the base class takes care of applying the filter at the pre, prior or post stage.
-
+///
+/// Note: a subclass of FilterBase (called `Filter` from now on) can opt to extract its
+/// settings either from a Configuration object or from a subclass of
+/// FilterParametersBaseWithAbstractAction (recommended).
+///
+/// In the former case, `Filter` should provide a constructor with the following signature:
+///
+///    Filter(ioda::ObsSpace &os, const eckit::Configuration &conf,
+///           std::shared_ptr<ioda::ObsDataVector<int> > qcflags,
+///           std::shared_ptr<ioda::ObsDataVector<float> > obserrors);
+///
+/// In the latter case, a subclass of FilterParametersBaseWithAbstractAction holding the settings
+/// of the filter in question should be defined. (Usually it can in fact be a subclass of
+/// FilterParametersBase; it is necessary to inherit directly from
+/// FilterParametersBaseWithAbstractAction only if it is not appropriate for `reject` to be the
+/// default action of the filter.) The `Filter` class should then typedef `Parameters_` to the name
+/// of that subclass and provide a constructor with the following signature:
+///
+///    Filter(ioda::ObsSpace &os, const Parameters_ &params,
+///           std::shared_ptr<ioda::ObsDataVector<int> > qcflags,
+///           std::shared_ptr<ioda::ObsDataVector<float> > obserrors);
+///
 class FilterBase : public ObsProcessorBase {
  public:
   FilterBase(ioda::ObsSpace &, const FilterParametersBaseWithAbstractAction &parameters,
