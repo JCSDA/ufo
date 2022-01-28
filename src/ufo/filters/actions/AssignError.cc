b2591d741cd52c2ef8607cd8d32a56afd3704dec
Greg Thompson <63315545+gthompsnJCSDA@users.noreply.github.com>
2022-01-28 13:35:09 -0700

    Bugfix: get the AssignError filter to respect where clause (#1856)
    * fixing bug described in issue number 37; solution by JJ Guerrette
    
    * put in a unit test
    
    * bug fix test name
    
    * adjust passedBenchmark in satwinds_gfs_qc
    
    * adjust passedBenchmark in aircraft_gfs_qc
    
    * update with the compare variables section
    
    * change up the variable names to try and be more clear
    
    * bug fix name of variable in TestReference
    
    Co-authored-by: gthompsnJCSDA <gthompsn@ucar.edu>
    Co-authored-by: Fabio L R Diniz <45880035+fabiolrdiniz@users.noreply.github.com>
    Co-authored-by: Ryan Honeyager <honeyage@ucar.edu>

--- src/ufo/filters/actions/AssignError.cc
+++ src/ufo/filters/actions/AssignError.cc
@@ -49,10 +49,10 @@ AssignError::AssignError(const Parameters_ & parameters)
 // -----------------------------------------------------------------------------
 
 void AssignError::apply(const Variables & vars,
-                        const std::vector<std::vector<bool>> &,
+                        const std::vector<std::vector<bool>> &mask,
                         const ObsFilterData & data,
                         int /*filterQCflag*/,
-                        ioda::ObsDataVector<int> & flags,
+                        ioda::ObsDataVector<int> & qcFlags,
                         ioda::ObsDataVector<float> & obserr) const {
   oops::Log::debug() << " AssignError input obserr: " << obserr << std::endl;
   const float missing = util::missingValue(missing);
@@ -61,9 +61,9 @@ void AssignError::apply(const Variables & vars,
     float error = *parameters_.errorParameter.value();
     for (size_t jv = 0; jv < vars.nvars(); ++jv) {
       size_t iv = obserr.varnames().find(vars.variable(jv).variable());
-      size_t kv = flags.varnames().find(vars.variable(jv).variable());
+      size_t kv = qcFlags.varnames().find(vars.variable(jv).variable());
       for (size_t jobs = 0; jobs < obserr.nlocs(); ++jobs) {
-        if (flags[kv][jobs] == QCflags::pass) obserr[iv][jobs] = error;
+        if (mask[jv][jobs] && qcFlags[kv][jobs] == QCflags::pass) obserr[iv][jobs] = error;
       }
     }
     // If variable is specified
@@ -88,9 +88,10 @@ void AssignError::apply(const Variables & vars,
     for (size_t jv = 0; jv < vars.nvars(); ++jv) {
       // find current variable index in obserr
       size_t iv = obserr.varnames().find(vars.variable(jv).variable());
-      size_t kv = flags.varnames().find(vars.variable(jv).variable());
+      size_t kv = qcFlags.varnames().find(vars.variable(jv).variable());
       for (size_t jobs = 0; jobs < obserr.nlocs(); ++jobs) {
-        if (flags[kv][jobs] == QCflags::pass && errors[error_jv[jv]][jobs] != missing)
+        if (mask[jv][jobs] && qcFlags[kv][jobs] == QCflags::pass
+                              && errors[error_jv[jv]][jobs] != missing)
           obserr[iv][jobs] = errors[error_jv[jv]][jobs];
       }
     }
