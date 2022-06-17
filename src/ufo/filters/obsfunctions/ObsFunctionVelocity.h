1828c4bc9e781183a407e241135feb48f572c869
James Cotton <64855970+james-cotton@users.noreply.github.com>
2022-06-17 14:51:14 +0100

    Add channels support for velocity obsfunction (#2044)
    * Based on Owen and Neill's branch
    
    * Add new test. Both 1d and 2d tests pass
    
    * Add description, tidy and coding norm pass
    
    * Tidy includes
    
    * Change variable names to newer format
    
    Co-authored-by: Ryan Honeyager <honeyage@ucar.edu>

--- src/ufo/filters/obsfunctions/ObsFunctionVelocity.h
+++ src/ufo/filters/obsfunctions/ObsFunctionVelocity.h
@@ -9,25 +9,68 @@
 #define UFO_FILTERS_OBSFUNCTIONS_OBSFUNCTIONVELOCITY_H_
 
 #include <string>
+#include <vector>
+
+#include "oops/util/parameters/Parameter.h"
+#include "oops/util/parameters/Parameters.h"
 
 #include "ufo/filters/obsfunctions/ObsFunctionBase.h"
 #include "ufo/filters/Variables.h"
 
 namespace ufo {
 
+class ObsFilterData;
+
+/// \brief Options controlling Velocity ObsFunction
+template <typename FunctionValue>
+class VelocityParameters : public oops::Parameters {
+  OOPS_CONCRETE_PARAMETERS(VelocityParameters, Parameters)
+
+ public:
+  /// List of channels available for assimilation
+  oops::Parameter<std::string> channelList{"channels", "", this};
+};
+
 // -----------------------------------------------------------------------------
 
-class ObsFunctionVelocity : public ObsFunctionBase<float> {
+/// \brief Outputs the wind speed of the u(eastward_wind) and v(northward_wind)
+/// components of wind.
+///
+/// Example 1
+///
+///  obs function:
+///    name: Velocity@ObsFunction
+///    options:
+///      type: ObsValue
+///
+/// will return sqrt( eastward_wind@ObsValue  * eastward_wind@ObsValue +
+///                   northward_wind@ObsValue * northward_wind@ObsValue )
+///
+/// Example 2 - multi-channel
+///
+///  obs function:
+///    name: Velocity@ObsFunction
+///    options:
+///      type: ObsValue
+///      channels: 1-4
+///
+/// will return sqrt( eastward_wind_<channel>@ObsValue  * eastward_wind_<channel>@ObsValue +
+///                   northward_wind_<channel>@ObsValue * northward_wind_<channel>@ObsValue )
+///
+
+template <typename FunctionValue>
+class Velocity : public ObsFunctionBase<FunctionValue> {
  public:
-  explicit ObsFunctionVelocity(const eckit::LocalConfiguration);
-  ~ObsFunctionVelocity();
+  explicit Velocity(const eckit::LocalConfiguration &);
 
   void compute(const ObsFilterData &,
-               ioda::ObsDataVector<float> &) const;
+               ioda::ObsDataVector<FunctionValue> &) const;
   const ufo::Variables & requiredVariables() const;
  private:
+  VelocityParameters<FunctionValue> options_;
   ufo::Variables invars_;
   std::string group_;
+  std::vector<int> channels_;
 };
 
 // -----------------------------------------------------------------------------
