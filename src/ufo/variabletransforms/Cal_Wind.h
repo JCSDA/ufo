de7a44e68898166ad41332063cc1f9091f5323b1
JoanneAWaller <52206323+JoanneAWaller@users.noreply.github.com>
2022-08-24 03:03:26 +0100

    Modify wind transforms to allow surface winds to be transformed (#2192)
    * Allow group name to be something other than ObsValue
    
    * Modified for Chris' comments
    
    * Update group-variable naming
    
    * Modify wind transforms to allow surface winds to be transformed
    
    * Add tests
    
    * Coding norms
    
    * Review comments and removal of wind_direction
    
    * DerivedPGD -> PGE
    
    * Revert "Review comments and removal of wind_direction"
    
    This reverts commit 89aef8947d232813a7205c9b60b76aa2babcb446.
    
    * Revert "Revert "Review comments and removal of wind_direction""
    
    This reverts commit 41d9208891b9b661c10ba0d252a61776cdab8459.
    
    * Revert "DerivedPGD -> PGE"
    
    This reverts commit b29bb7d9d687a469a000b47f4b20729f07145457.
    
    Co-authored-by: james-cotton <james.cotton@metoffice.gov.uk>
    Co-authored-by: Patrick Nichols <patjnichols@gmail.com>
    Co-authored-by: Benjamin Ruston <93737224+BenjaminRuston@users.noreply.github.com>

--- src/ufo/variabletransforms/Cal_Wind.h
+++ src/ufo/variabletransforms/Cal_Wind.h
@@ -25,6 +25,10 @@ class Cal_WindComponentsParameters: public VariableTransformParametersBase {
  public:
   /// Observation group name. Default is ObsValue.
   oops::Parameter<std::string> group{"group", "ObsValue", this};
+  oops::Parameter<std::string> WindDirectionVariable{"wind direction variable",
+                                                     "wind_from_direction", this};
+  oops::Parameter<std::string> WindSpeedVariable{"wind speed variable",
+                                                "wind_speed", this};
 };
 
 /// Configuration parameters for the wind speed and direction transform.
@@ -34,6 +38,10 @@ class Cal_WindSpeedAndDirectionParameters: public VariableTransformParametersBas
  public:
   /// Observation group name. Default is ObsValue.
   oops::Parameter<std::string> group{"group", "ObsValue", this};
+  oops::Parameter<std::string> EastwardWindVariable{"eastward wind variable",
+                                                    "eastward_wind", this};
+  oops::Parameter<std::string> NorthwardWindVariable{"northward wind variable",
+                                                     "northward_wind", this};
 };
 
 /*!
@@ -58,6 +66,8 @@ class Cal_WindSpeedAndDirection : public TransformBase {
   void runTransform(const std::vector<bool> &apply) override;
 
  private:
+  std::string eastwardwindvariable_;
+  std::string northwardwindvariable_;
   /// Group name.
   std::string group_;
 };
@@ -86,6 +96,8 @@ class Cal_WindComponents : public TransformBase {
   void runTransform(const std::vector<bool> &apply) override;
 
  private:
+  std::string windspeedvariable_;
+  std::string winddirectionvariable_;
   /// Group name.
   std::string group_;
 };
