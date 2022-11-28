2598dfa3d937ac7b71e117cd2700ad36d8199e9d
Michael Cooke <48374999+mikecooke77@users.noreply.github.com>
2022-11-28 17:15:52 +0000

    Get more ctests working (#2502)
    Co-authored-by: Patrick Nichols <patjnichols@gmail.com>

--- src/ufo/filters/obsfunctions/SetSurfaceType.h
+++ src/ufo/filters/obsfunctions/SetSurfaceType.h
@@ -48,8 +48,7 @@ class SetSurfaceTypeParameters : public oops::Parameters {
   /// Default is 'MetaData/land_sea'
   /// Example: to set to SSMIS report type (which is MetaData/surfaceQualifier) set
   ///          SurfaceReport Name: MetaData/surfaceQualifier
-  oops::Parameter<std::string> SurfaceMetaDataName{"SurfaceReport Name",
-                                                   "MetaData/surfaceQualifier", this};
+  oops::Parameter<std::string> SurfaceMetaDataName{"SurfaceReport Name", "MetaData/landOrSeaQualifier", this};
 
   /// Use reported Surface Elevation (default false)
   /// Example: To use the AAPPreported elevation set
