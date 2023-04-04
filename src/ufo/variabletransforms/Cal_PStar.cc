96bc3815933d055311400899a7013573dd669553
JoanneAWaller <jo.waller@metoffice.gov.uk>
2023-04-04 13:16:35 +0100

    Additional modifications to Obs error passing

--- src/ufo/variabletransforms/Cal_PStar.cc
+++ src/ufo/variabletransforms/Cal_PStar.cc
@@ -172,6 +172,12 @@ void Cal_PStar::runTransform(const std::vector<bool> &apply) {
 
   putObservation("surfacePressure", PStar);
   obsdb_.put_db("DerivedObsError", "surfacePressure", PStar_error);
+  const size_t iv = obserr_.varnames().find("surfacePressure");
+  for (size_t jobs = 0; jobs < obsdb_.nlocs(); ++jobs) {
+    if (!apply[jobs])
+      continue;
+    obserr_[iv][jobs] = PStar_error[jobs];
+  }
   obsdb_.put_db("GrossErrorProbability", "surfacePressure", PStar_PGE);
   obsdb_.put_db("DiagnosticFlags/PmslUsed", "surfacePressure", PmslUsed_flag);
   obsdb_.put_db("DiagnosticFlags/PstdUsed", "surfacePressure", PstdUsed_flag);
