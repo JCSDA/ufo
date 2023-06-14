72268aa8dbb542047203a1b92cae051d1462f093
DrTVockerodtMO <114994380+DrTVockerodtMO@users.noreply.github.com>
2023-06-14 20:04:03 +0100

    [feature] Add reconditioning to ObsErrorCrossVarCov (#2729)
    * IN PROGRESS: add reconditioning to ObsErrorCrossVarCov
    
    * Removed vestigial nused_locs from ObsErrorCrossVarCov.cc
    
    * Removed extra spaces on line 117 of ObsErrorCrossVarCov.cc
    
    * adding recondition parameters and test
    
    * add simple test case
    
    * Fixed nused = 0 SEGFAULT errors, added logging
    
    Added logging messages and a guard clause to only perform the Eigen operations if nused >0 to prevent segfaults.
    
    * Fixed bug where top left most entry of the R matrix became zero, leading to non positive definite R matrix
    
    * point to correct obs file
    
    * add ridge regression and ME methods. Add tests comparing to known output. Add threshold as optional parameter
    
    * Moved reconditioning into nused > 0 guard clause, changed ridge regression implementation to be more consistent with design philosophy
    
    * Matched #ifndef name with #define in Reconditioning.h, changed formatting of log messages, replaced std::cout instances with oops::Log::info()
    
    * Changed formatting of log messages
    
    * Delete temporary file LastTest.log
    
    * Added crown copyright
    
    * Changed KFrac to kmax in minimum eigenvalue reconditioning
    
    * Update ObsErrorCrossVarCov.cc
    
    Fixed instances where varcorrelations_ was being accessed with incorrect indices
    
    * Update ObsErrorCrossVarCov.cc
    
    Changed var_ind to ivar for conciseness.
    
    * Update CrossVarCov.cc to catch unviable kmax, update test .nc4
    
    * Fixed log messages failing ufo_coding_norms
    
    * Update ObsErrorCrossVarCov.cc
    
    Re-implemented nused_locs to prevent locations not processed from contaminating the correlations.
    
    * Update ObsErrorCrossVarCov.cc
    
    Added guard clause to reassignment of varcorrelations_, such that if no locations are used the varcorrelation_ remains unchanged.
    
    * Update ObsErrorCrossVarCov.cc
    
    Deleted whitespaces (ufo_coding_norms)
    
    * Added if block for option selection in ridge regression
    
    * Added shift option to ridge regression. Added more tests to increase code coverage
    
    * Reconditioning.h include statements now in alphabetical order
    
    * Update Reconditioning.h
    
    Reconditioning.h include statements now in alphabetical order
    
    * Moved nc4 files from ufo to ufo-data, updated test yaml
    
    * Trigger CI
    
    * Update ObsErrorCrossVarCov.cc
    
    Changed messages from info to debug to prevent spam.
    
    * Update ObsErrorCrossVarCov.h
    
    Added name of the first argument of recondition into the header signature.
    
    * Addressed some of reviewer comments
    
    * Renamed unit vector to sample vector, made logging output statements more accurate
    
    * Changed instances of oops::mpi::world() to oops::mpi::myself() in Reconditioning.h, added MPI reconditioning test
    
    * Refactored reconditioning parameter check in constructor
    
    * Changed spaces of length 4 to length 2 for indented statements
    
    * Update ObsErrorCrossVarCov.cc
    
    Changed spaces of length 4 to length 2 for indented statements
    
    * Update Reconditioning.h
    
    Changed spaces of length 4 to length 2 for indented statements
    
    * Trigger CI
    
    * Update ObsErrorCrossVarCov.cc
    
    Added guard for if an invalid recondition method is provided.
    
    * Update reconditioning.yaml
    
    Added invalid reconditioning method test for coverage.
    
    * Added enumerator for recondition method parameter
    
    * Should now pass ufo_coding_norms
    
    * Addressed reviewer comments
    
    ---------
    
    Co-authored-by: jrucinskiMO <james.rucinski@metoffice.gov.uk>
    Co-authored-by: Patrick Nichols <patjnichols@gmail.com>

--- src/ufo/errors/ObsErrorCrossVarCov.h
+++ src/ufo/errors/ObsErrorCrossVarCov.h
@@ -1,5 +1,6 @@
 /*
  * (C) Copyright 2021 UCAR.
+ * (C) Crown copyright 2023 Met Office
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -17,8 +18,8 @@
 
 #include "oops/base/Variables.h"
 #include "oops/interface/ObsErrorBase.h"
+#include "oops/util/parameters/OptionalParameter.h"
 #include "oops/util/parameters/Parameters.h"
-#include "oops/util/parameters/RequiredParameter.h"
 
 #include "ufo/ObsTraits.h"
 
@@ -28,6 +29,52 @@ namespace ioda {
 
 namespace ufo {
 
+enum class ReconditionMethod {
+  MINIMUMEIGENVALUE, RIDGEREGRESSION, NORECONDITIONING
+};
+
+struct ReconditionMethodParameterTraitsHelper {
+  typedef ReconditionMethod EnumType;
+  static constexpr char enumTypeName[] = "ReconditionMethod";
+  static constexpr util::NamedEnumerator<ReconditionMethod> namedValues[] = {
+    { ReconditionMethod::MINIMUMEIGENVALUE, "Minimum Eigenvalue" },
+    { ReconditionMethod::RIDGEREGRESSION, "Ridge Regression" },
+    { ReconditionMethod::NORECONDITIONING, "No reconditioning" }
+  };
+};
+
+}  // namespace ufo
+
+namespace oops {
+
+template <>
+struct ParameterTraits<ufo::ReconditionMethod> :
+    public EnumParameterTraits<ufo::ReconditionMethodParameterTraitsHelper>
+{};
+
+}  // namespace oops
+
+namespace ufo {
+/// \brief Parameters for Recondition method
+class ReconditionParameters : public oops::ObsErrorParametersBase {
+    OOPS_CONCRETE_PARAMETERS(ReconditionParameters, ObsErrorParametersBase)
+ public:
+  /// Method of reconditioning, either Ridge regression or minimum eigenvalue
+  oops::Parameter<ReconditionMethod> ReconMethod{"recondition method",
+                                                 "recondition method (options:"
+                                                 " Minimum Eigenvalue,"
+                                                 " Ridge Regression,"
+                                                 " No reconditioning)",
+                                                 ReconditionMethod::NORECONDITIONING,
+                                                 this};
+  /// Target fraction of condition number
+  oops::OptionalParameter<float> kFrac{"fraction", this};
+  /// Threshold for Minimum EigenValue
+  oops::OptionalParameter<float> Threshold{"threshold", this};
+  /// Shift for Ridge Regression
+  oops::OptionalParameter<float> Shift{"shift", this};
+};
+
 /// \brief Parameters for obs errors with cross-variable correlations
 class ObsErrorCrossVarCovParameters : public oops::ObsErrorParametersBase {
   OOPS_CONCRETE_PARAMETERS(ObsErrorCrossVarCovParameters, ObsErrorParametersBase)
@@ -35,8 +82,9 @@ class ObsErrorCrossVarCovParameters : public oops::ObsErrorParametersBase {
   /// Input file containing correlations or covariances. If covariances are
   /// specified, they will be converted to correlations.
   oops::RequiredParameter<std::string> inputFile{"input file", this};
-};
 
+  oops::OptionalParameter<ReconditionParameters> reconditioning{"reconditioning", this};
+};
 // -----------------------------------------------------------------------------
 /// \brief Observation error covariance matrix with cross-variable
 ///        correlations.
@@ -45,7 +93,9 @@ class ObsErrorCrossVarCovParameters : public oops::ObsErrorParametersBase {
 ///          deviations are read from ObsSpace as ObsError group.
 ///          Full observation error covariance matrix is R = D^{1/2} * C * D^{1/2}
 ///          where D^{1/2} is a diagonal matrix with stddev_ (ObsError group)
-///          on the diagonal, and C is the correlation matrix.
+///          on the diagonal, and C is the correlation matrix. The cross-variable
+///          R matrices at each location can be reconditioned to reduce their condition
+///          number, in order to speed up convergence of minimisation.
 class ObsErrorCrossVarCov : public oops::interface::ObsErrorBase<ObsTraits> {
  public:
   /// The type of parameters passed to the constructor.
@@ -58,6 +108,9 @@ class ObsErrorCrossVarCov : public oops::interface::ObsErrorBase<ObsTraits> {
 
   /// Update obs error standard deviations to be equal to \p stddev
   void update(const ioda::ObsVector & stddev) override;
+
+  /// Recondition the R matrix.
+  void recondition(const Parameters_ & options, const ioda::ObsVector & mask);
 
   /// Multiply \p y by this observation error covariance
   /// Computed as R * dy = D^{1/2} * C * D^{1/2} * dy
