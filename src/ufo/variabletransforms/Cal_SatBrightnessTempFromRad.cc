5c24424103b775ce13427e6dcf4bd1b144f7ff43
Michael Cooke <48374999+mikecooke77@users.noreply.github.com>
2023-09-19 18:51:13 +0100

    Allow radiance to BT transform to take wavenumber with size Channel (#3014)
    * Update to be able to use spectral variable as iloc,ichan or ichan
    
    * Add comment to explain if loop
    
    * Update to resolve coding norms failure
    
    * empty commit to kick off test again
    
    * trigger CI
    
    * trigger CIU
    
    ---------
    
    Co-authored-by: Patrick Nichols <patjnichols@gmail.com>
    Co-authored-by: Benjamin Ruston <93737224+BenjaminRuston@users.noreply.github.com>
    Co-authored-by: BenjaminRuston <benr@ucar.edu>

--- src/ufo/variabletransforms/Cal_SatBrightnessTempFromRad.cc
+++ src/ufo/variabletransforms/Cal_SatBrightnessTempFromRad.cc
@@ -47,16 +47,29 @@ void Cal_SatBrightnessTempFromRad::runTransform(const std::vector<bool> &apply) 
   ioda::ObsDataVector<float> radiance(obsdb_, radianceVar);
   data_.get(parameters_.transformVariable.value(), radiance);
 
-  // Read in the spectral variable
-  oops::Variables spectralVar(parameters_.spectralVariable.value().toOopsVariables());
-  ioda::ObsDataVector<float> spectralVariable(obsdb_, spectralVar);
-  data_.get(parameters_.spectralVariable.value(), spectralVariable);
-
   // Setup Variables
   const size_t nlocs = obsdb_.nlocs();
   const size_t nvars = radiance.nvars();
   float minval = parameters_.minvalue.value().value_or(missingValueFloat);
   float maxval = parameters_.maxvalue.value().value_or(missingValueFloat);
+
+  // Read in the spectral variable
+  std::vector<float> spectralVariable;
+  // This if clause is only true if the specal variable is of size Channel.  If it not then the
+  // variables are stored in the obsspace as var_channel e.g. sensorWavenumber_16 size Location
+  if (obsdb_.getObsGroup().vars.exists(parameters_.spectralVariable.value().fullName())) {
+    std::vector<float> buf;
+    obsdb_.getObsGroup().vars[parameters_.spectralVariable.value().fullName()].read<float>(buf);
+    for (size_t ichan = 0; ichan < nvars; ++ichan) {
+      spectralVariable.push_back(buf[ichan]);
+    }
+  } else {
+    ioda::ObsDataVector<float> buf(obsdb_, parameters_.spectralVariable.value().toOopsVariables());
+    data_.get(parameters_.spectralVariable.value(), buf);
+    for (size_t ichan = 0; ichan < nvars; ++ichan) {
+      spectralVariable.push_back(buf[ichan][0]);
+    }
+  }
 
   // Create output array
   std::vector<std::vector<float>> brightnessTemperature(
@@ -71,18 +84,18 @@ void Cal_SatBrightnessTempFromRad::runTransform(const std::vector<bool> &apply) 
       for (size_t ichan = 0; ichan < nvars; ++ichan) {
         if ( radiance[ichan][iloc] != missingValueFloat &&
              radiance[ichan][iloc] > 0.0f &&
-             spectralVariable[ichan][iloc] != missingValueFloat) {
+             spectralVariable[ichan] != missingValueFloat) {
           double bt = missingValueFloat;
           switch (parameters_.radianceUnits.value()) {
             case RadianceUnits::WAVENUMBER: {
               bt = formulas::inversePlanck(static_cast<double>(radiance[ichan][iloc]),
-                                           static_cast<double>(spectralVariable[ichan][iloc]),
+                                           static_cast<double>(spectralVariable[ichan]),
                                            parameters_.planck1.value(),
                                            parameters_.planck2.value());
               break;
             }
             case RadianceUnits::FREQUENCY: {
-              double freq = static_cast<double>(spectralVariable[ichan][iloc]);
+              double freq = static_cast<double>(spectralVariable[ichan]);
               double wvn = freq / Constants::speedOfLight;  // Hz to m-1
               double rad = static_cast<double>(radiance[ichan][iloc]) * freq / wvn;
               bt = formulas::inversePlanck(rad, wvn, parameters_.planck1.value(),
@@ -90,7 +103,7 @@ void Cal_SatBrightnessTempFromRad::runTransform(const std::vector<bool> &apply) 
               break;
             }
             case RadianceUnits::WAVELENGTH: {
-              double wvl = static_cast<double>(spectralVariable[ichan][iloc]);
+              double wvl = static_cast<double>(spectralVariable[ichan]);
               double wvn = 1.0e6 / wvl;  // microns to m-1
               double rad = static_cast<double>(radiance[ichan][iloc]) * wvl / wvn;
               bt = formulas::inversePlanck(rad, wvn, parameters_.planck1.value(),
